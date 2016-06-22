72ea2f598e9ba841de3dfd49861247676ebf3a0b
jakubkirylo <jakub.kirylo@gmail.com>
2016-06-22 22:18:25 +0200

    School and arrays

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -18,3 +18,33 @@ $("#header").prepend(formattedName);
 
 var skills = ["awesomeness","programming","team leading"];
 $("#main").append(skills);
+
+var bio = {
+	name: "Jakub",
+	lastname: "Kirylo",
+	role: "Programmer",
+	pictureURL: "http://farm4.static.flickr.com/3114/2524849923_1c191ef42e.jpg",
+	welcomeMsg: "Hello!",
+	skills: ["C#", "HTML", "CSS", "SQL"]
+};
+
+$("#main").append(bio.name);
+
+var work = new Object();
+work.current = "Junior SQL programmer";
+work.employer = "Brandbank";
+work.years = 0;
+work.city = "Warsaw";
+
+function education(name,years,city)
+{
+	this.name = name;
+	this.years = years;
+	this.city = city;
+}
+
+;
+var myEdycation = new education("WWSI",1,"Warsaw");
+
+$("#main").append(work["current"]);
+$("#main").append(myEdycation.name);
