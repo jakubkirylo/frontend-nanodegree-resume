3258c30b42380a3c15a1a808f6c9e02997a73d1b
jakubkirylo <jakub.kirylo@gmail.com>
2016-06-22 20:18:55 +0200

    Lesson1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,3 +2,11 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 $("#main").append("Jakub Kiryło");
+
+var awesomeThougths = "I'm Jakub and I am AWESOME";
+
+console.log(awesomeThougths);
+
+var funThoughts = awesomeThougths.replace("AWESOME","FUN");
+
+$("#main").append(funThoughts);
